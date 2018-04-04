55cc2b6a5e6c054b0953e1bfa3371208c72220c8
Hackbright Student <no-reply@hackbrightacademy.com>
2018-04-04 16:05:35 -0700

    Add variable operator

--- calculator.py
+++ calculator.py
@@ -6,34 +6,42 @@ calculator program yourself in this file.
 
 from arithmetic import *
 
+
 def calculator_2():
+
+    """ calculator function takes input from the user in format
+    operator, number, number and returns result"""
+
     calculating = True
 
     while calculating:
         user_input = raw_input()
         user_input = user_input.split(" ")
+        operator = user_input[0]
 
-        for each in range(1,len(user_input)):
-            user_input[each] = int(user_input[each])
-        
-        print user_input
-        if user_input[0] == "+":
+        #num1 = user_input[1]
+        #num2 = user_input[2]
+
+        for each in range(1, len(user_input)):
+            user_input[each] = float(user_input[each])
+
+        if operator == "+":
             result = add(user_input[1], user_input[2])
-        elif user_input[0] == "-":
+        elif operator == "-":
             result = subtract(user_input[1], user_input[2])
-        elif user_input[0] == "/":
+        elif operator == "/":
             result = divide(user_input[1], user_input[2])
-        elif user_input[0] == "*":
+        elif operator == "*":
             result = multiply(user_input[1], user_input[2])
-        elif user_input[0] == "square":
-            result = square(user_input[1])        
-        elif user_input[0] == "cube":
+        elif operator == "square":
+            result = square(user_input[1])
+        elif operator == "cube":
             result = cube(user_input[1])
-        elif user_input[0] == "pow":
+        elif operator == "pow":
             result = power(user_input[1], user_input[2])
-        elif user_input[0] == "mod":
+        elif operator == "mod":
             result = mod(user_input[1], user_input[2])
-        elif user_input[0] == "q" or user_input[0] == "quit":
+        elif operator == "q" or operator == "quit":
             print "Quitting Program"
             calculating = False
         else:
