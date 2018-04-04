b25ad4dc219c3b483fd8c5c2c47ac3701c1be457
Hackbright Student <no-reply@hackbrightacademy.com>
2018-04-04 15:42:28 -0700

    Arithmetic functions working in calculator

---
+++ calculator.py
@@ -0,0 +1,44 @@
+"""A prefix-notation calculator.
+
+Using the arithmetic.py file from Calculator Part 1, create the
+calculator program yourself in this file.
+"""
+
+from arithmetic import *
+
+def calculator_2():
+    calculating = True
+
+    while calculating:
+        user_input = raw_input()
+        user_input = user_input.split(" ")
+
+        user_input[1] = int(user_input[1])
+        user_input[2] = int(user_input[2])
+
+        print user_input
+        if user_input[0] == "+":
+            result = add(user_input[1], user_input[2])
+        elif user_input[0] == "-":
+            result = subtract(user_input[1], user_input[2])
+        elif user_input[0] == "/":
+            result = divide(user_input[1], user_input[2])
+        elif user_input[0] == "*":
+            result = multiply(user_input[1], user_input[2])
+        elif user_input[0] == "square":
+            result = square(user_input[1])        
+        elif user_input[0] == "cube":
+            result = cube(user_input[1])
+        elif user_input[0] == "pow":
+            result = power(user_input[1], user_input[2])
+        elif user_input[0] == "mod":
+            result = mod(user_input[1], user_input[2])
+        elif user_input[0] == "q" or user_input[0] == "quit":
+            print "Quitting Program"
+            calculating = False
+        else:
+            print "Please input valid things"
+
+        print result  
+
+calculator_2()           
