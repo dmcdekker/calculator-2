d39a76c177c3856479568c413991c565331f28b9
Hackbright Student <no-reply@hackbrightacademy.com>
2018-04-04 17:42:22 -0700

    Ensure correct number of numbers for a given command

--- calculator.py
+++ calculator.py
@@ -15,11 +15,11 @@ def calculator_2():
     calculating = True
 
     while calculating:
-        user_input = raw_input('Please enter an operator followed by numbers:')
+        user_input = raw_input('Please enter an operator followed by numbers:\n')
         user_input = user_input.split(" ")
         operator = user_input[0]
 
-        if validate_operator(operator) and validate_list_length(user_input) and validate_digits(user_input):
+        if validate_operator(operator, ['+', '-', '/', '*', 'pow', 'mod']) and validate_list_length(user_input, 3) and validate_digits(user_input):
 
             for each in range(1, len(user_input)):
                     user_input[each] = float(user_input[each])
@@ -32,27 +32,30 @@ def calculator_2():
                 result = divide(user_input[1], user_input[2])
             elif operator == "*":
                 result = multiply(user_input[1], user_input[2])
-            elif operator == "square":
-                result = square(user_input[1])
-            elif operator == "cube":
-                result = cube(user_input[1])
+            
             elif operator == "pow":
                 result = power(user_input[1], user_input[2])
             elif operator == "mod":
                 result = mod(user_input[1], user_input[2])
-            elif operator == "q" or operator == "quit":
-                print "Quitting Program"
-                calculating = False
+            print result    
+        elif validate_operator(operator, ['square', 'cube']) and validate_list_length(user_input, 2) and validate_digits(user_input):
+            for each in range(1, len(user_input)):
+                user_input[each] = float(user_input[each])
+
+            if operator == "square":
+                result = square(user_input[1])    
+            elif operator == "cube":
+                result = cube(user_input[1])
             print result
         elif operator == "q" or operator == "quit":
                 print "Quitting Program"
                 calculating = False
         else:
-            print "Please input valid format: operator number number"
+            print "Please input valid format: operator number number\n"
 
 
-def validate_operator(op):
-    valid_operators = ['+', '-', '/', '*', 'square', 'cube', 'pow', 'mod']
+def validate_operator(op, valid_operators):
+    
 
     if op in valid_operators:
         return True
@@ -60,8 +63,8 @@ def validate_operator(op):
         return False
 
 
-def validate_list_length(user_list):
-    return len(user_list) <= 3
+def validate_list_length(user_list, list_length):
+    return len(user_list) == list_length
 
 
 def validate_digits(user_list):
